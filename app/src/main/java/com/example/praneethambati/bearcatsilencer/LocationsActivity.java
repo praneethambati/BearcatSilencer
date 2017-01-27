1b3a8c49d84d7042b5a328657165f93dfa50b1fb
Praneeth <ambatisaipraneeth@gmail.com>
2017-01-27 15:00:26 -0600

    Changes in LocationsActivity

--- app/src/main/java/com/example/praneethambati/bearcatsilencer/LocationsActivity.java
+++ app/src/main/java/com/example/praneethambati/bearcatsilencer/LocationsActivity.java
@@ -1,7 +1,13 @@
 package com.example.praneethambati.bearcatsilencer;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,17 +25,22 @@ import android.widget.Toast;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class LocationsActivity extends AppCompatActivity {
+public class LocationsActivity extends AppCompatActivity{
+
 
     MyCustomAdapter dataAdapter = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations);
 
+
         displayListView();
 
         checkButtonClick();
+
     }
     private void displayListView() {
 
@@ -66,7 +77,8 @@ public class LocationsActivity extends AppCompatActivity {
 }
 
 
-private class MyCustomAdapter extends ArrayAdapter<LocationsModel> {
+
+    private class MyCustomAdapter extends ArrayAdapter<LocationsModel> {
 
     private ArrayList<LocationsModel> locationsList;
 
